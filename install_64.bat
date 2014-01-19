5a1fbe9850ef1113cbe615e71f96d69d3ffd1354
Michal Borejszo <spitfire05pl@gmail.com>
2014-01-19 13:53:18 +0100

    create gh-pages

--- install_64.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-C:\Windows\Microsoft.NET\Framework64\v2.0.50727\RegAsm.exe -codebase swsp.dll
-if errorlevel 1 goto fail
-cscript MessageBox.vbs "Installed for 64 bit SolidWorks. If you change the location of this addin, please re-run the install script."
-exit
-:fail
-cscript MessageBox.vbs "Installation failed!"
